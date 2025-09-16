352c716eafc6625c2e487ec33f093a4d3620457e
Peyno <75258235+Peyno@users.noreply.github.com>
2025-09-16 19:19:30 +0200

    finishing Video 32

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-user';
+import { Component, computed, Input, input } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -10,15 +8,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); 
 
-//  get imagePath() {
-//    return 'assets/users/' + this.selectedUser.avatar;
-//  }
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  onSelectUer() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-     this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUer() {}
 }
